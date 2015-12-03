b9562e74b86bac35400724ca1edd50d41388f32b
Marco Pracucci <marco.pracucci@spreaker.com>
2015-12-03 18:38:53 +0100

    Fixed config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
         "consistent-return": 1,
         "curly": 1,
         "dot-notation": 0,
-        "no-alert": 1
+        "no-alert": 1,
         "no-console": 1,
         "no-eval": 1,
         "no-multi-spaces": 0,
