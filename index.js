69e8fee1f2f168c0c8fb774114188be6ad956b90
Marco Pracucci <marco.pracucci@spreaker.com>
2015-12-03 18:29:23 +0100

    Added more rules

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = {
     "rules": {
         "strict": 0,
-        "no-console": 0,
+        "consistent-return": 1,
+        "curly": 1,
+        "dot-notation": 0,
+        "no-alert": 1
+        "no-console": 1,
+        "no-eval": 1,
         "no-multi-spaces": 0,
         "no-underscore-dangle": 0,
-        "dot-notation": 0
     }
 };
