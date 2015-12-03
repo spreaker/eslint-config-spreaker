4fa3b630d180ecfb1ae4f928a1c4382160a6e802
Marco Pracucci <marco.pracucci@spreaker.com>
2015-12-03 18:06:27 +0100

    Initial import

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "rules": {
+        "strict": 0,
+        "no-console": 0,
+        "no-multi-spaces": 0,
+        "no-underscore-dangle": 0,
+        "dot-notation": 0
+    }
+};
